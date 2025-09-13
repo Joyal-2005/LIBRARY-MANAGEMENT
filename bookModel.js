8cdb12ea5e040f58d1543d0d6415008bf412d4f8
Joyal-2005 <joyalaugustin17@gmail.com>
2025-09-13 08:54:20 +0530

    Update bookModel.js

--- bookModel.js
+++ bookModel.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-  name: { type: String, required: true },  //  required field
+  name: { type: String, required: true },  
   author: { type: String },
   category: { type: String },
   price: { type: Number }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
+
